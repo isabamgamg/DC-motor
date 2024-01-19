6fff45c534f5a8a52bbc92d9beb5434b6b2bff3a
isabamgamg <rainbowgt7@naver.com>
2024-01-19 11:17:34 +0900

    all

---
+++ ssd1306/Core/Inc/hangulStringTable.h
@@ -0,0 +1,14 @@
+/*
+ * hangulStringTable.h
+ *
+ *  Created on: Jan 19, 2024
+ *      Author: npedu-203-admin
+ */
+
+#ifndef INC_HANGULSTRINGTABLE_H_
+#define INC_HANGULSTRINGTABLE_H_
+
+const char hello[] = {"다연"};
+const char string2[] = {"대한민국"};
+
+#endif /* INC_HANGULSTRINGTABLE_H_ */
